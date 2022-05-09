7df97e16a77e737c07a90a5a3b9a03110a3d22f9
Dave <akkradet@profilegroup-hrs.com>
2022-05-09 18:01:09 +0700

    style mat tab

--- apps/uni-service/src/app/app.module.ts
+++ apps/uni-service/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule, AppRoutingModule],
+  imports: [BrowserModule, RouterModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- libs/uni-service/feature/req-degree-cert/src/lib/req-degree-cert-home/req-degree-cert-home.component.ts
+++ libs/uni-service/feature/req-degree-cert/src/lib/req-degree-cert-home/req-degree-cert-home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ksp-req-degree-cert-home',
   templateUrl: './req-degree-cert-home.component.html',
   styleUrls: ['./req-degree-cert-home.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ReqDegreeCertHomeComponent implements OnInit {
   constructor() {}
