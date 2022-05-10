d1c8834e145b489c8d14f09aa80a90f7ecd05c9b
Dew4356 <dulyanut.nitmai@kkumail.com>
2022-05-10 15:04:10 +0700

    add step 2 tab 1

---
+++ libs/uni-service/ui/forms/src/lib/req-degree-cert-step-two-tab-two/req-degree-cert-step-two-tab-two.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ksp-req-degree-cert-step-two-tab-two',
+  templateUrl: './req-degree-cert-step-two-tab-two.component.html',
+  styleUrls: ['./req-degree-cert-step-two-tab-two.component.scss'],
+})
+export class ReqDegreeCertStepTwoTabTwoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
