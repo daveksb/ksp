064e03c6bda7e152e4672638bac0ff180220f344
Dave <akkradet@profilegroup-hrs.com>
2022-05-17 15:12:19 +0700

    finish retired main flow

--- libs/uni-service/feature/retired/src/lib/retired-attachment/retired-attachment.component.ts
+++ libs/uni-service/feature/retired/src/lib/retired-attachment/retired-attachment.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { RetiredConfirmComponent } from '../retired-confirm/retired-confirm.component';
 
 @Component({
   selector: 'ksp-retired-attachment',
   templateUrl: './retired-attachment.component.html',
   styleUrls: ['./retired-attachment.component.scss'],
 })
-export class RetiredAttachmentComponent implements OnInit {
-  constructor() {}
+export class RetiredAttachmentComponent {
+  constructor(private router: Router, public dialog: MatDialog) {}
 
-  ngOnInit(): void {}
+  prevPage() {
+    this.router.navigate(['/', 'retired', 'reason']);
+  }
+
+  cancel() {
+    this.router.navigate(['/', 'retired', 'search']);
+  }
+
+  save() {
+    const dialogRef = this.dialog.open(RetiredConfirmComponent, {
+      height: '300px',
+      width: '400px',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
