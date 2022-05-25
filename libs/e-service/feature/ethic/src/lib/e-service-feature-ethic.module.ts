bba57713d87cada949562ef13cfb7321a2ebbafd
Dave <akkradet@profilegroup-hrs.com>
2022-05-25 14:36:56 +0700

    add route

--- libs/e-service/feature/ethic/src/lib/e-service-feature-ethic.module.ts
+++ libs/e-service/feature/ethic/src/lib/e-service-feature-ethic.module.ts
@@ -1,17 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AccusationListComponent } from './accusation-list/accusation-list.component';
 import { AccusationRecordComponent } from './accusation-record/accusation-record.component';
+import { EServiceContainerPageComponent } from '@ksp/e-service/feature/container-page';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: EServiceContainerPageComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full',
+      },
+      {
+        path: 'list',
+        component: AccusationListComponent,
+      },
+      {
+        path: 'record',
+        component: AccusationRecordComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-  imports: [
-    CommonModule,
-
-    RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
-    ]),
-  ],
+  imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: [AccusationListComponent, AccusationRecordComponent],
   exports: [AccusationListComponent, AccusationRecordComponent],
 })
