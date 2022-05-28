9e7ecce8907fc0b5fdaf73f2e9e4ab41d4eb88b5
Dew4356 <dulyanut.nitmai@kkumail.com>
2022-05-28 11:53:00 +0700

    uni degree cert step 1

---
+++ libs/uni-service/feature/degree-cert/src/lib/step-four/step-four.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ksp-step-four',
+  templateUrl: './step-four.component.html',
+  styleUrls: ['./step-four.component.scss'],
+})
+export class StepFourComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ libs/uni-service/feature/degree-cert/src/lib/step-three/step-three.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ksp-step-three',
+  templateUrl: './step-three.component.html',
+  styleUrls: ['./step-three.component.scss'],
+})
+export class StepThreeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
