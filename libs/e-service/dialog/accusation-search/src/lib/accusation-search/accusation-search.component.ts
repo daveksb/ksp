63bb6cecee90e9946f8152940004670b142ee99a
Dave <akkradet@profilegroup-hrs.com>
2022-05-30 13:17:42 +0700

    add table

--- libs/e-service/dialog/accusation-search/src/lib/accusation-search/accusation-search.component.ts
+++ libs/e-service/dialog/accusation-search/src/lib/accusation-search/accusation-search.component.ts
@@ -1,11 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PersonLicense {
+  id: string;
+  personId: string;
+  name: string;
+  organization: string;
+  school: string;
+  province: string;
+}
+
+export const data = [
+  {
+    id: 1,
+    personId: '110200051214',
+    name: 'พรทิพย์ นาคปรก',
+    organization: 'xxxx',
+    school: 'วิทยาลัยเทคนิค ฉะเชิงเทรา',
+    province: 'ฉะเชิงเทรา',
+  },
+  {
+    id: 2,
+    personId: '110200051214',
+    name: 'พรทิพย์ นาคปรก',
+    organization: 'xxxx',
+    school: 'วิทยาลัยเทคนิค ฉะเชิงเทรา',
+    province: 'ฉะเชิงเทรา',
+  },
+];
+
 @Component({
   selector: 'e-service-ethic-ui-accusation-list',
   templateUrl: './accusation-search.component.html',
   styleUrls: ['./accusation-search.component.scss'],
 })
 export class AccusationSearchComponent implements OnInit {
+  displayedColumns: string[] = [
+    'personId',
+    'name',
+    'organization',
+    'school',
+    'province',
+  ];
+  dataSource = data;
+
   constructor() {}
 
   ngOnInit(): void {}
--- libs/e-service/dialog/accusation-search/src/lib/e-service-dialog-accusation-search.module.ts
+++ libs/e-service/dialog/accusation-search/src/lib/e-service-dialog-accusation-search.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccusationSearchComponent } from './accusation-search/accusation-search.component';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   declarations: [AccusationSearchComponent],
   exports: [AccusationSearchComponent],
 })
