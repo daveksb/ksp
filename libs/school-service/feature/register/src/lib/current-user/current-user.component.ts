7982bc11c89ad7a6cf8bfbc1df2e8b12cf3420b7
Dew4356 <dulyanut.nitmai@kkumail.com>
2022-05-31 15:11:33 +0700

    current user style

--- libs/school-service/feature/register/src/lib/current-user/current-user.component.ts
+++ libs/school-service/feature/register/src/lib/current-user/current-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { UniversitySearchComponent } from '@ksp/shared/ui/university-search';
 
 @Component({
   selector: 'school-service-current-user',
@@ -7,9 +9,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./current-user.component.scss'],
 })
 export class CurrentUserComponent {
-  constructor(public router: Router) {}
+  constructor(public router: Router, private dialog: MatDialog) {}
+
+  openSearchDialog() {
+    this.dialog.open(UniversitySearchComponent, {
+      height: '900px',
+      width: '1200px',
+    });
+  }
 
   next() {
-      this.router.navigate(['/', 'register', 'register']);
+    this.router.navigate(['/', 'register', 'register']);
   }
 }
