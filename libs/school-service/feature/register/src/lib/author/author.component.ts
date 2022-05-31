9f6bffc59ea17f36002e3eea1bb29b335d700996
Dew4356 <dulyanut.nitmai@kkumail.com>
2022-05-31 16:36:19 +0700

    school author

--- libs/school-service/feature/register/src/lib/author/author.component.ts
+++ libs/school-service/feature/register/src/lib/author/author.component.ts
@@ -1,12 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import {
+  CompleteDialogComponent,
+  ConfirmDialogComponent,
+} from '@ksp/shared/ui/dialog';
 
 @Component({
-  selector: 'ksp-author',
+  selector: 'school-service--author',
   templateUrl: './author.component.html',
   styleUrls: ['./author.component.scss'],
 })
-export class AuthorComponent implements OnInit {
-  constructor() {}
+export class AuthorComponent {
+  constructor(private router: Router, public dialog: MatDialog) {}
 
-  ngOnInit(): void {}
+  cancel() {
+    this.router.navigate(['/', 'login']);
+  }
+
+  back() {
+    this.router.navigate(['/', 'register', 'register']);
+  }
+
+  save() {
+    const confirmDialog = this.dialog.open(ConfirmDialogComponent, {
+      height: '200px',
+      width: '350px',
+      data: {
+        title: `คุณต้องการยืนยันข้อมูลใช่หรือไม่?`,
+        subTitle: `คุณยืนยันข้อมูลและส่งเรื่องเพื่อขออนุมัติ
+        ใช่หรือไม`,
+      },
+    });
+
+    confirmDialog.componentInstance.confirmed.subscribe((res) => {
+      if (res) {
+        this.onCompleted();
+      }
+    });
+  }
+
+  onCompleted() {
+    const completeDialog = this.dialog.open(CompleteDialogComponent, {
+      height: '300px',
+      width: '375px',
+      data: {
+        header: `ยืนยันข้อมูลสำเร็จ`,
+        content: `วันที่ : 10 ตุลาคม  2565
+        เลขที่ใบคำขอ : 12234467876543`,
+        subContent: `กรุณาตรวจสอบสถานะใบคำขอผ่านทางอีเมล
+        ผู้ที่ลงทะเบียนภายใน 3 วันทำการ`,
+
+        buttonLabel: 'กลับสู่หน้าหลัก',
+      },
+    });
+
+    completeDialog.componentInstance.completed.subscribe((res) => {
+      if (res) {
+        this.router.navigate(['/', 'login']);
+      }
+    });
+  }
 }
