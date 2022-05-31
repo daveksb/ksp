0b1bcdbe30bb43d50693fe9c3091b1d34cf7d0ea
Dave <akkradet@profilegroup-hrs.com>
2022-05-31 14:52:21 +0700

    add dialog

--- libs/self-service/feature/master-page/src/lib/self-service-master-page/self-service-master-page.component.ts
+++ libs/self-service/feature/master-page/src/lib/self-service-master-page/self-service-master-page.component.ts
@@ -1,6 +1,89 @@
 import { Component } from '@angular/core';
 import { MenuConfig } from '@ksp/shared/ui/side-menu';
 
+export const menu: MenuConfig[] = [
+  {
+    icon: 'assets/images/icon-sidenav/home.svg ',
+    label: 'หน้าแรก',
+    path: '',
+  },
+  {
+    icon: 'assets/images/icon-sidenav/paper.svg',
+    label: 'ยื่นใบคำขอ',
+    path: '',
+  },
+  {
+    icon: 'assets/images/icon-sidenav/card.svg',
+    label: 'ใบอนุญาตประกอบวิชาชีพ',
+    path: '',
+    subMenuName: 'license',
+    isExpanded: false,
+    subMenu: [
+      {
+        label: 'ขอขึ้นทะเบียนใบอนุญาตประกอบวิชาชีพ',
+        path: 'request',
+      },
+      {
+        label: 'ขอต่ออายุใบประกอบวิชาชีพ',
+        path: '',
+      },
+      {
+        label: 'ขอใบแทนใบอนุญาตประกอบวิชาชีพ',
+        path: '',
+      },
+      {
+        label: '##ช่องทางชำระเงิน',
+        path: 'payment-channel',
+      },
+      {
+        label: '##แก้ไขใบอนุญาต',
+        path: 'edit',
+      },
+    ],
+  },
+  {
+    icon: 'assets/images/icon-sidenav/event.svg',
+    label: 'กิจกรรมการพัฒนาตัวเอง',
+    path: '',
+  },
+  {
+    icon: 'assets/images/icon-sidenav/reward.svg',
+    label: 'รางวัลของฉัน',
+    path: '',
+  },
+  {
+    icon: 'assets/images/icon-sidenav/people.svg',
+    label: 'ข้อมูลของฉัน',
+    path: '',
+    subMenuName: 'myProfile',
+    subMenu: [
+      {
+        label: 'ข้อมูลส่วนตัว',
+        path: '',
+      },
+      {
+        label: 'สถานที่ทำงาน',
+        path: '',
+      },
+      {
+        label: 'ข้อมูลการศึกษา',
+        path: '',
+      },
+      {
+        label: 'ข้อมูลประสบการณ์วิชาชีพ',
+        path: '',
+      },
+      {
+        label: 'ข้อมูลผลการประเมินสมรรถนะ',
+        path: '',
+      },
+      {
+        label: 'ประวัติการชำระเงินและใบเสร็จรับเงิน',
+        path: 'payment-history',
+      },
+    ],
+  },
+];
 @Component({
   selector: 'ksp-self-service-master-page',
   templateUrl: './self-service-master-page.component.html',
@@ -10,88 +93,6 @@ export class SelfServiceMasterPageComponent {
   menuConfig: MenuConfig[];
 
   constructor() {
-    this.menuConfig = [
-      {
-        icon: 'assets/images/icon-sidenav/home.svg ',
-        label: 'หน้าแรก',
-        path: '',
-      },
-      {
-        icon: 'assets/images/icon-sidenav/paper.svg',
-        label: 'ยื่นใบคำขอ',
-        path: '',
-      },
-      {
-        icon: 'assets/images/icon-sidenav/card.svg',
-        label: 'ใบอนุญาตประกอบวิชาชีพ',
-        path: '',
-        subMenuName: 'license',
-        isExpanded: false,
-        subMenu: [
-          {
-            label: 'ขอขึ้นทะเบียนใบอนุญาตประกอบวิชาชีพ',
-            path: 'request',
-          },
-          {
-            label: 'ขอต่ออายุใบประกอบวิชาชีพ',
-            path: '',
-          },
-          {
-            label: 'ขอใบแทนใบอนุญาตประกอบวิชาชีพ',
-            path: '',
-          },
-          {
-            label: '##ช่องทางชำระเงิน',
-            path: 'payment-channel',
-          },
-          {
-            label: '##แก้ไขใบอนุญาต',
-            path: 'edit',
-          },
-        ],
-      },
-      {
-        icon: 'assets/images/icon-sidenav/event.svg',
-        label: 'กิจกรรมการพัฒนาตัวเอง',
-        path: '',
-      },
-      {
-        icon: 'assets/images/icon-sidenav/reward.svg',
-        label: 'รางวัลของฉัน',
-        path: '',
-      },
-      {
-        icon: 'assets/images/icon-sidenav/people.svg',
-        label: 'ข้อมูลของฉัน',
-        path: '',
-        subMenuName: 'myProfile',
-        subMenu: [
-          {
-            label: 'ข้อมูลส่วนตัว',
-            path: '',
-          },
-          {
-            label: 'สถานที่ทำงาน',
-            path: '',
-          },
-          {
-            label: 'ข้อมูลการศึกษา',
-            path: '',
-          },
-          {
-            label: 'ข้อมูลประสบการณ์วิชาชีพ',
-            path: '',
-          },
-          {
-            label: 'ข้อมูลผลการประเมินสมรรถนะ',
-            path: '',
-          },
-          {
-            label: 'ประวัติการชำระเงินและใบเสร็จรับเงิน',
-            path: 'payment-history',
-          },
-        ],
-      },
-    ];
+    this.menuConfig = menu;
   }
 }
--- libs/self-service/ui/forms/src/lib/form-user-workplace/form-user-workplace.component.ts
+++ libs/self-service/ui/forms/src/lib/form-user-workplace/form-user-workplace.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UniversitySearchComponent } from '@ksp/shared/ui/university-search';
 
 @Component({
   selector: 'ksp-form-user-workplace',
   templateUrl: './form-user-workplace.component.html',
   styleUrls: ['./form-user-workplace.component.css'],
 })
-export class FormUserWorkplaceComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class FormUserWorkplaceComponent {
+  constructor(private dialog: MatDialog) {}
+  openSearchDialog() {
+    this.dialog.open(UniversitySearchComponent, {
+      height: '900px',
+      width: '1200px',
+    });
+  }
 }
