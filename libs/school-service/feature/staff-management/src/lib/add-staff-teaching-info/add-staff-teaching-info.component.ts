8dac9fd48781f853e6e57030a6c1469fee7cf831
Dave <akkradet@profilegroup-hrs.com>
2022-06-01 15:54:47 +0700

    add components

---
+++ libs/school-service/feature/staff-management/src/lib/add-staff-teaching-info/add-staff-teaching-info.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'school-service-add-staff-teaching-info',
+  templateUrl: './add-staff-teaching-info.component.html',
+  styleUrls: ['./add-staff-teaching-info.component.scss'],
+})
+export class AddStaffTeachingInfoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- libs/school-service/feature/staff-management/src/lib/school-service-feature-staff-management.module.ts
+++ libs/school-service/feature/staff-management/src/lib/school-service-feature-staff-management.module.ts
@@ -4,6 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { StaffListComponent } from './staff-list/staff-list.component';
 import { SchoolServiceContainerPageComponent } from '@ksp/school-service/feature/container-page';
 import { LicenseSearchComponent } from './license-search/license-search.component';
+import { SharedUiTopNavModule } from '@ksp/shared/ui/top-nav';
+import { AddStaffPersonInfoComponent } from './add-staff-person-info/add-staff-person-info.component';
+import { AddStaffTeachingInfoComponent } from './add-staff-teaching-info/add-staff-teaching-info.component';
 
 export const routes: Routes = [
   {
@@ -13,13 +16,28 @@ export const routes: Routes = [
       { path: '', redirectTo: 'list', pathMatch: 'full' },
       { path: 'list', component: StaffListComponent },
       { path: 'license-search', component: LicenseSearchComponent },
+      { path: 'add-staff-person-info', component: AddStaffPersonInfoComponent },
+      {
+        path: 'add-staff-teaching-info',
+        component: AddStaffTeachingInfoComponent,
+      },
     ],
   },
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
-  declarations: [StaffListComponent, LicenseSearchComponent],
-  exports: [StaffListComponent, LicenseSearchComponent],
+  imports: [CommonModule, SharedUiTopNavModule, RouterModule.forChild(routes)],
+  declarations: [
+    StaffListComponent,
+    LicenseSearchComponent,
+    AddStaffPersonInfoComponent,
+    AddStaffTeachingInfoComponent,
+  ],
+  exports: [
+    StaffListComponent,
+    LicenseSearchComponent,
+    AddStaffPersonInfoComponent,
+    AddStaffTeachingInfoComponent,
+  ],
 })
 export class SchoolServiceFeatureStaffManagementModule {}
