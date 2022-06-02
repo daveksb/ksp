5d24dd69806b65363b3dc4e7f27de521e3985043
Dave <akkradet@profilegroup-hrs.com>
2022-06-02 18:37:53 +0700

    fix button

--- libs/school-service/feature/forget-password/src/lib/set-new-password/set-new-password.component.ts
+++ libs/school-service/feature/forget-password/src/lib/set-new-password/set-new-password.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import {
