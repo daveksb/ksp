d0dd8dfd79168c8ad7c9a3250ce8f378aefc1451
Dave <akkradet@profilegroup-hrs.com>
2022-06-06 18:52:26 +0700

    remove close function

--- libs/self-service/ui/forms/src/lib/form-user-performance/form-user-performance.component.ts
+++ libs/self-service/ui/forms/src/lib/form-user-performance/form-user-performance.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'ksp-form-user-performance',
+  selector: 'self-service-form-user-performance',
   templateUrl: './form-user-performance.component.html',
   styleUrls: ['./form-user-performance.component.css'],
 })
--- libs/self-service/ui/forms/src/lib/form-user-workplace/form-user-workplace.component.ts
+++ libs/self-service/ui/forms/src/lib/form-user-workplace/form-user-workplace.component.ts
@@ -3,12 +3,13 @@ import { MatDialog } from '@angular/material/dialog';
 import { UniversitySearchComponent } from '@ksp/shared/ui/university-search';
 
 @Component({
-  selector: 'ksp-form-user-workplace',
+  selector: 'self-service-form-user-workplace',
   templateUrl: './form-user-workplace.component.html',
   styleUrls: ['./form-user-workplace.component.css'],
 })
 export class FormUserWorkplaceComponent {
   constructor(private dialog: MatDialog) {}
+
   openSearchDialog() {
     this.dialog.open(UniversitySearchComponent, {
       height: '900px',
