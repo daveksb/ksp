ff2102b48e9b76aaf04cc2b228cd00252f57aa9a
Dave <akkradet@profilegroup-hrs.com>
2022-06-06 17:46:06 +0700

    rename

---
+++ libs/self-service/feature/license/src/lib/license-edit/license-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'self-service-license-edit',
+  templateUrl: './license-edit.component.html',
+  styleUrls: ['./license-edit.component.css'],
+})
+export class LicenseEditComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ libs/self-service/feature/license/src/lib/license-request/license-request.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'self-service-license-request',
+  templateUrl: './license-request.component.html',
+  styleUrls: ['./license-request.component.css'],
+})
+export class LicenseRequestComponent {}
