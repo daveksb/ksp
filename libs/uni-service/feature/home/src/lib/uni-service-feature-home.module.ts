c32a2e7b41b409660cff1979be7eb4237a1579ff
Dew4356 <dulyanut.nitmai@kkumail.com>
2022-06-07 17:19:16 +0700

    edit bottom uni service

--- libs/uni-service/feature/home/src/lib/uni-service-feature-home.module.ts
+++ libs/uni-service/feature/home/src/lib/uni-service-feature-home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UniServiceHomeComponent } from './uni-service-home/uni-service-home.component';
 import { SelfServiceUiFormsModule } from '@ksp/self-service/ui/forms';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  imports: [CommonModule, SelfServiceUiFormsModule],
+  imports: [CommonModule, SelfServiceUiFormsModule, MatTableModule],
   declarations: [UniServiceHomeComponent],
   exports: [UniServiceHomeComponent],
 })
