3227856ffdec8f40ca74a0a121f1da5ccde6ce72
Dew4356 <dulyanut.nitmai@kkumail.com>
2022-06-07 18:31:46 +0700

    exp form

--- libs/shared/form/education-level/src/index.ts
+++ libs/shared/form/education-level/src/index.ts
@@ -7,3 +7,5 @@ export * from './lib/education-level-form-two/education-level-form-two.component
 export * from './lib/education-level-form-three/education-level-form-three.component';
 
 export * from './lib/education-level-form-four/education-level-form-four.component';
+
+export * from './lib/experience-form/experience-form.component';
