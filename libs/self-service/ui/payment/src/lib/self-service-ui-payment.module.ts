fd2c4be6d9066e8030e95437ec9ff3bee71a42f0
Dew4356 <dulyanut.nitmai@kkumail.com>
2022-06-07 15:25:11 +0700

    view receive

--- libs/self-service/ui/payment/src/lib/self-service-ui-payment.module.ts
+++ libs/self-service/ui/payment/src/lib/self-service-ui-payment.module.ts
@@ -4,10 +4,16 @@ import { PaymentChannelComponent } from './payment-channel/payment-channel.compo
 import { SelfServiceUiMenuModule } from '@ksp/self-service/ui/menu';
 import { SharedUiTopNavModule } from '@ksp/shared/ui/top-nav';
 import { PromptpayComponent } from './promptpay/promptpay.component';
+import { ViewReceiveComponent } from './view-receive/view-receive.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule, SelfServiceUiMenuModule, SharedUiTopNavModule],
-  declarations: [PaymentChannelComponent, PromptpayComponent],
-  exports: [PaymentChannelComponent, PromptpayComponent],
+  imports: [CommonModule, SelfServiceUiMenuModule, SharedUiTopNavModule, MatIconModule],
+  declarations: [
+    PaymentChannelComponent,
+    PromptpayComponent,
+    ViewReceiveComponent,
+  ],
+  exports: [PaymentChannelComponent, PromptpayComponent, ViewReceiveComponent],
 })
 export class SelfServiceUiPaymentModule {}
