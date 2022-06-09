187e50c8be5fc22d0b71a0ad1ba7a407ff3ceb6b
Dave <akkradet@profilegroup-hrs.com>
2022-06-09 11:27:34 +0700

    add row button

---
+++ libs/shared/ui/add-row-button/README.md
@@ -0,0 +1,3 @@
+# shared-ui-add-row-button
+
+This library was generated with [Nx](https://nx.dev).
---
+++ libs/shared/ui/add-row-button/src/lib/add-row-button/add-row-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ksp-add-row-button',
+  templateUrl: './add-row-button.component.html',
+  styleUrls: ['./add-row-button.component.scss'],
+})
+export class AddRowButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
