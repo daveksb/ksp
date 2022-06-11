4594f5eed2e32a580bbeabcd1506fba46017e948
Dew4356 <dulyanut.nitmai@kkumail.com>
2022-06-11 16:49:35 +0700

    edit

--- libs/shared/form/degree-cert-step-two/src/lib/advisor/advisor.component.ts
+++ libs/shared/form/degree-cert-step-two/src/lib/advisor/advisor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'ksp-step-2-advisor',
@@ -6,7 +8,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advisor.component.scss'],
 })
 export class AdvisorComponent implements OnInit {
-  constructor() {}
+  advisorForm = this.fb.group({
+    generalInfo: [],
+    hasMoreCourses: [],
 
-  ngOnInit(): void {}
+    courses: this.fb.array([
+      this.fb.group({
+        courseName: [''],
+        studentNumber: [''],
+        advisorStatus: [''],
+        advisorType: [''],
+      }),
+    ]),
+  });
+
+  mainForm = this.fb.group({
+    advisors: this.fb.array([this.advisorForm]),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.mainForm.valueChanges.pipe(debounceTime(750)).subscribe((res) => {
+      console.log('form value = ', res);
+    });
+  }
+
+  addCourse(index: number) {
+    const form = this.fb.group({
+      courseName: [''],
+      studentNumber: [''],
+      advisorStatus: [''],
+      advisorType: [''],
+    });
+    this.getCourses(index).push(form);
+  }
+
+  addAdvisor() {
+    const advisorForm = this.fb.group({
+      generalInfo: [],
+      hasMoreCourses: [],
+      courses: this.fb.array([
+        this.fb.group({
+          courseName: [''],
+          studentNumber: [''],
+          advisorStatus: [''],
+          advisorType: [''],
+        }),
+      ]),
+    });
+
+    this.advisors.push(advisorForm);
+  }
+
+  getCourses(index: number) {
+    return this.mainForm.controls['advisors'].controls[index].controls[
+      'courses'
+    ];
+  }
+
+  getHasMoreCourses(index: number) {
+    return this.mainForm.controls['advisors'].controls[index].controls[
+      'hasMoreCourses'
+    ].value;
+  }
+
+  get advisors() {
+    return this.mainForm.controls['advisors'];
+  }
 }
