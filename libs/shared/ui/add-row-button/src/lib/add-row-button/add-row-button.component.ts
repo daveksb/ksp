e546b012b51b29b2b61f16232ee5ee64f98234c2
Dave <akkradet@profilegroup-hrs.com>
2022-06-11 16:52:17 +0700

    add nitet

--- libs/shared/ui/add-row-button/src/lib/add-row-button/add-row-button.component.ts
+++ libs/shared/ui/add-row-button/src/lib/add-row-button/add-row-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'ksp-add-row-button',
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class AddRowButtonComponent {
   @Output() addClick = new EventEmitter<boolean>();
+  @Input() mode = 'default';
 
   onClick() {
     this.addClick.emit(true);
