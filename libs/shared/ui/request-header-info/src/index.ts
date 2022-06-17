560ad2788e1436394a5955cec96569d8ca600f8b
Dave <akkradet@profilegroup-hrs.com>
2022-06-17 14:15:23 +0700

    gen component

---
+++ libs/shared/ui/request-header-info/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/shared-ui-request-header-info.module';
+
+export * from './lib/request-header-info/request-header-info.component';
---
+++ libs/shared/ui/request-header-info/src/lib/request-header-info/request-header-info.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ksp-request-header-info',
+  templateUrl: './request-header-info.component.html',
+  styleUrls: ['./request-header-info.component.scss'],
+  standalone: true,
+})
+export class RequestHeaderInfoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
