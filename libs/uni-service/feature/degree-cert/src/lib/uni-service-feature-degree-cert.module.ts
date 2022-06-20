13d08d55161c340d22fd74b6ece95343bee3853a
Dew4356 <dulyanut.nitmai@kkumail.com>
2022-06-20 17:39:59 +0700

    stepper edit

--- libs/uni-service/feature/degree-cert/src/lib/uni-service-feature-degree-cert.module.ts
+++ libs/uni-service/feature/degree-cert/src/lib/uni-service-feature-degree-cert.module.ts
@@ -14,6 +14,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { SharedFormDegreeCertStepThreeModule } from '@ksp/shared/form/degree-cert/step-three';
 import { SharedFormDegreeCertStepOneModule } from '@ksp/shared/form/degree-cert/step-one';
 import { RequestHeaderInfoComponent } from '@ksp/shared/ui/request-header-info';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [
   {
@@ -47,6 +48,7 @@ const routes: Routes = [
     MatStepperModule,
     MatTabsModule,
     RequestHeaderInfoComponent,
+    MatIconModule
   ],
   declarations: [DegreeCertComponent],
 })
