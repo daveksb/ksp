da03741c7a0f65734c1f32aeb294259676b97b60
Dave <akkradet@profilegroup-hrs.com>
2022-06-21 17:38:53 +0700

    uni select

--- libs/shared/form/university-select/src/lib/university-select/university-select.component.ts
+++ libs/shared/form/university-select/src/lib/university-select/university-select.component.ts
@@ -1,24 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
 import { UniversitySearchComponent } from '@ksp/shared/form/university-search';
 
 @Component({
   selector: 'ksp-university-select',
   templateUrl: './university-select.component.html',
   styleUrls: ['./university-select.component.scss'],
+  standalone: true,
 })
 export class UniversitySelectComponent {
-  constructor(private router: Router, public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog) {}
 
   search() {
-    const dialogRef = this.dialog.open(UniversitySearchComponent, {
+    this.dialog.open(UniversitySearchComponent, {
       height: '900px',
       width: '1200px',
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
     });
   }
 }
