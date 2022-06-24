1b59f99825e3f49f1d9a2f081a342475f5973ae9
Dave <akkradet@profilegroup-hrs.com>
2022-06-24 12:34:19 +0700

    rename nitet

---
+++ libs/shared/form/teacher-info/src/lib/less-exp-nitet/less-exp-nitet.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ksp-less-exp-nitet',
+  templateUrl: './less-exp-nitet.component.html',
+  styleUrls: ['./less-exp-nitet.component.scss'],
+})
+export class LessExpNitetComponent {
+  constructor() {}
+}
