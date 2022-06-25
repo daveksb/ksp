959448a8c916e6290c7e66ffee2565701d0f06d1
Dave <akkradet@profilegroup-hrs.com>
2022-06-25 15:58:19 +0700

    use bootstrap utility class

--- libs/shared/directive/src/index.ts
+++ libs/shared/directive/src/index.ts
@@ -3,5 +3,4 @@ export * from './lib/shared-directive.module';
 export * from './lib/dynamic-component.directive';
 export * from './lib/hide-in-view-mode.directive';
 export * from './lib/set-gray-background.directive';
-export * from './lib/set-white-background.directive';
 export * from './lib/sticky-bottom-nav.directive';
