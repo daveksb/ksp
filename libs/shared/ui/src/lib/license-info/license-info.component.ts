82fbf19b34a87f5597f246a5d8bf80e5eede5976
Dave <akkradet@profilegroup-hrs.com>
2022-06-26 17:18:12 +0700

    license info

--- libs/shared/ui/src/lib/license-info/license-info.component.ts
+++ libs/shared/ui/src/lib/license-info/license-info.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ksp-license-info',
   templateUrl: './license-info.component.html',
   styleUrls: ['./license-info.component.scss'],
   standalone: true,
+  imports: [CommonModule],
 })
 export class LicenseInfoComponent {
+  @Input() isLicenseRevoked = false;
+
   constructor() {}
 }
