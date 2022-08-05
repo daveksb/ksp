d1f7951f0d886ee69174db63252fdfef0b693b6f
Dave <akkradet@profilegroup-hrs.com>
2022-08-05 15:56:54 +0700

    license info input

--- libs/self-service/ui/src/lib/license-info/license-info.component.ts
+++ libs/self-service/ui/src/lib/license-info/license-info.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'self-service-license-info',
   templateUrl: './license-info.component.html',
-  styleUrls: ['./license-info.component.css'],
+  styleUrls: ['./license-info.component.scss'],
   imports: [CommonModule],
   standalone: true,
 })
-export class SelfServiceLicenseInfoComponent {}
+export class SelfServiceLicenseInfoComponent {
+  @Input() date = '';
+  @Input() requestNo = '';
+  @Input() KuruspaId = '';
+  @Input() personId = '';
+}
