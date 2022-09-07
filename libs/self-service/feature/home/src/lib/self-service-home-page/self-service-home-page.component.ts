c516e1e45597686805943de2cb9b20664edd1fcc
dew4356 <dulyanut.nitmai@kkumail.com>
2022-09-08 00:06:17 +0700

    edit

--- libs/self-service/feature/home/src/lib/self-service-home-page/self-service-home-page.component.ts
+++ libs/self-service/feature/home/src/lib/self-service-home-page/self-service-home-page.component.ts
@@ -45,39 +45,11 @@ export class SelfServiceHomePageComponent {
     this.router.navigate(['/', 'renew-license', 'request']);
   }
 
-  /* xxx() {
-    this.router.navigate(['/', 'reward', 'detail']);
-  } */
-
   rewardPage(requestType: number) {
     this.router.navigate(['/', 'reward', 'request'], {
       queryParams: { type: requestType },
     });
   }
-
-  /* reward1() {
-    this.router.navigate(['/', 'reward', 'council']);
-  }
-
-  reward2() {
-    this.router.navigate(['/', 'reward', 'thai-teacher']);
-  }
-
-  reward3() {
-    this.router.navigate(['/', 'reward', 'best-teacher']);
-  }
-
-  reward4() {
-    this.router.navigate(['/', 'reward', 'praise-teacher']);
-  }
-
-  reward5() {
-    this.router.navigate(['/', 'reward', 'senior-teacher']);
-  }
-
-  reward6() {
-    this.router.navigate(['/', 'reward', 'research']);
-  } */
 
   refundFee() {
     this.router.navigate(['/', 'refund-fee', 'request']);
