4912753d5ee12d4aa167ad2c97d282bd8b7a0244
Nutti Saelor <leelorz6@gmail.com>
2022-09-18 00:28:42 +0700

    feature: create request for school manager

--- libs/self-service/feature/license/src/lib/license-request-school-manager/license-request-school-manager.component.ts
+++ libs/self-service/feature/license/src/lib/license-request-school-manager/license-request-school-manager.component.ts
@@ -4,7 +4,24 @@ import { Router } from '@angular/router';
 import { UserInfoFormType } from '@ksp/shared/constant';
 import { ConfirmDialogComponent } from '@ksp/shared/dialog';
 import { ForbiddenPropertyFormComponent } from '@ksp/shared/form/others';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {
+  AddressService,
+  GeneralInfoService,
+  EducationDetailService,
+  MyInfoService,
+  LicenseRequestService as RequestLicenseService,
+} from '@ksp/shared/service';
+import {
+  replaceEmptyWithNull,
+  toLowercaseProp,
+  parseJson,
+} from '@ksp/shared/utility';
+import { defaultRequestPayload } from '@ksp/shared/interface';
 
+@UntilDestroy()
 @Component({
   selector: 'self-service-license-request-school-manager',
   templateUrl: './license-request-school-manager.component.html',
@@ -24,23 +41,207 @@ export class LicenseRequestSchoolManagerComponent implements OnInit {
     '2. เอกสารผู้สำเร็จการศึกษา ( ระบบ KSP BUNDIT)		',
     '3. วุฒิบัตรอบรม',
   ];
-  constructor(public dialog: MatDialog, private router: Router) {}
 
-  ngOnInit(): void {}
+  form = this.fb.group({
+    userInfo: [],
+    address1: [],
+    address2: [],
+    workplace: [],
+    education: [],
+    experience: [],
+  });
+  prefixList$!: Observable<any>;
+  nationalitys$!: Observable<any>;
+  provinces1$!: Observable<any>;
+  provinces2$!: Observable<any>;
+  provinces3$!: Observable<any>;
+  amphurs1$!: Observable<any>;
+  tumbols1$!: Observable<any>;
+  amphurs2$!: Observable<any>;
+  tumbols2$!: Observable<any>;
+  amphurs3$!: Observable<any>;
+  tumbols3$!: Observable<any>;
+  bureau$!: Observable<any>;
+
+  disableNextButton = false;
+
+  constructor(
+    public dialog: MatDialog,
+    private router: Router,
+    private fb: FormBuilder,
+    private generalInfoService: GeneralInfoService,
+    private addressService: AddressService,
+    private educationDetailService: EducationDetailService,
+    private myInfoService: MyInfoService,
+    private requestService: RequestLicenseService
+  ) {}
+
+  ngOnInit(): void {
+    this.getListData();
+    this.getMyInfo();
+    this.checkButtonsDisableStatus();
+  }
+
+  getListData() {
+    this.prefixList$ = this.generalInfoService.getPrefix();
+    this.nationalitys$ = this.generalInfoService.getNationality();
+    this.provinces1$ = this.addressService.getProvinces();
+    this.provinces2$ = this.provinces1$;
+    this.provinces3$ = this.provinces1$;
+    this.bureau$ = this.educationDetailService.getBureau();
+  }
+
+  getMyInfo() {
+    this.myInfoService.getMyInfo().subscribe((res) => {
+      this.patchUserInfo(res);
+      this.patchAddress(parseJson(res.addressinfo));
+      if (res.schooladdrinfo) {
+        this.patchWorkplace(parseJson(res.schooladdrinfo));
+      }
+    });
+  }
+
+  patchUserInfo(data: any) {
+    const {
+      birthdate,
+      phone,
+      email,
+      firstnameen,
+      firstnameth,
+      idcardno,
+      lastnameen,
+      lastnameth,
+      prefixen,
+      prefixth,
+      id,
+    } = data;
+    const patchData = {
+      birthdate: birthdate.split('T')[0],
+      contactphone: phone,
+      email,
+      firstnameen,
+      firstnameth,
+      idcardno,
+      lastnameen,
+      lastnameth,
+      prefixen,
+      prefixth,
+      id,
+    } as any;
+    this.form.controls.userInfo.patchValue(patchData);
+  }
+
+  patchAddress(addrs: any[]) {
+    //console.log('address = ', addrs);
+    if (addrs && addrs.length) {
+      addrs.map((addr: any, i: number) => {
+        if (i === 0) {
+          this.amphurs1$ = this.addressService.getAmphurs(addr.province);
+          this.tumbols1$ = this.addressService.getTumbols(addr.amphur);
+          this.form.controls.address1.patchValue(addr);
+        }
+        if (i === 1) {
+          this.amphurs2$ = this.addressService.getAmphurs(addr.province);
+          this.tumbols2$ = this.addressService.getTumbols(addr.amphur);
+          this.form.controls.address2.patchValue(addr);
+        }
+      });
+    }
+  }
+
+  patchWorkplace(data: any) {
+    console.log(data);
+    this.amphurs3$ = this.addressService.getAmphurs(data.province);
+    this.tumbols3$ = this.addressService.getTumbols(data.district);
+    this.form.controls.workplace.patchValue(data);
+  }
+
+  provinceChanged(addrType: number, evt: any) {
+    const province = evt.target?.value;
+    if (province) {
+      if (addrType === 1) {
+        this.amphurs1$ = this.addressService.getAmphurs(province);
+      } else if (addrType === 2) {
+        this.amphurs2$ = this.addressService.getAmphurs(province);
+      } else if (addrType === 3) {
+        this.amphurs3$ = this.addressService.getAmphurs(province);
+      }
+    }
+  }
+
+  amphurChanged(addrType: number, evt: any) {
+    const amphur = evt.target?.value;
+    if (amphur) {
+      if (addrType === 1) {
+        this.tumbols1$ = this.addressService.getTumbols(amphur);
+      } else if (addrType === 2) {
+        this.tumbols2$ = this.addressService.getTumbols(amphur);
+      } else if (addrType === 3) {
+        this.tumbols3$ = this.addressService.getTumbols(amphur);
+      }
+    }
+  }
+
+  useSameAddress(evt: any) {
+    const checked = evt.target.checked;
+    this.amphurs2$ = this.amphurs1$;
+    this.tumbols2$ = this.tumbols1$;
+    this.provinces2$ = this.provinces1$;
+    if (checked) {
+      this.form.controls.address2.patchValue(this.form.controls.address1.value);
+    }
+  }
+
+  createRequest(forbidden: any, currentProcess: string) {
+    const baseForm = this.fb.group(defaultRequestPayload);
+    const formData: any = this.form.getRawValue();
+    if (formData?.address1?.addressType) formData.address1.addresstype = 1;
+    if (formData?.address2?.addressType) formData.address2.addresstype = 2;
+
+    const { id, ...rawUserInfo } = formData.userInfo;
+    const userInfo = toLowercaseProp(rawUserInfo);
+
+    userInfo.ref1 = '1';
+    userInfo.ref2 = '01';
+    userInfo.ref3 = '2';
+    userInfo.systemtype = '1';
+    userInfo.requesttype = '1';
+    userInfo.subtype = '5';
+
+    const { educationType, educationLevelForm } = formData.education;
+
+    const payload = {
+      ...replaceEmptyWithNull(userInfo),
+      ...{
+        addressinfo: JSON.stringify([formData.address1, formData.address2]),
+      },
+      ...{ schooladdrinfo: JSON.stringify(formData.workplace) },
+      ...{ eduinfo: JSON.stringify({ educationType, ...educationLevelForm }) },
+      ...{
+        experienceinfo: JSON.stringify(formData.experience),
+      },
+      ...{ prohibitproperty: JSON.stringify(forbidden) },
+    };
+    payload.currentprocess = currentProcess;
+    console.log(payload);
+    baseForm.patchValue(payload);
+    return baseForm.value;
+  }
 
   save() {
+    console.log(this.form.value);
     const confirmDialog = this.dialog.open(ForbiddenPropertyFormComponent, {
       width: '900px',
     });
 
     confirmDialog.componentInstance.confirmed.subscribe((res) => {
       if (res) {
-        this.onCompleted();
+        this.onCompleted(res);
       }
     });
   }
 
-  onCompleted() {
+  onCompleted(forbidden: any) {
     const completeDialog = this.dialog.open(ConfirmDialogComponent, {
       width: '350px',
       data: {
@@ -52,14 +253,32 @@ export class LicenseRequestSchoolManagerComponent implements OnInit {
 
     completeDialog.componentInstance.saved.subscribe((res) => {
       if (res) {
-        this.router.navigate(['/', 'renew-license', 'request']);
+        const payload = this.createRequest(forbidden, '0');
+        this.requestService.requestLicense(payload).subscribe((res) => {
+          console.log('request result = ', res);
+          if (res.returncode === '00') {
+            this.router.navigate(['/home']);
+          }
+        });
       }
     });
 
     completeDialog.componentInstance.confirmed.subscribe((res) => {
       if (res) {
-        this.router.navigate(['/', 'license', 'payment-channel']);
+        const payload = this.createRequest(forbidden, '1');
+        this.requestService.requestLicense(payload).subscribe((res) => {
+          console.log('request result = ', res);
+          if (res.returncode === '00') {
+            this.router.navigate(['/license', 'payment-channel']);
+          }
+        });
       }
+    });
+  }
+
+  checkButtonsDisableStatus() {
+    this.form.valueChanges.pipe(untilDestroyed(this)).subscribe((res) => {
+      this.disableNextButton = !this.form.valid;
     });
   }
 }
