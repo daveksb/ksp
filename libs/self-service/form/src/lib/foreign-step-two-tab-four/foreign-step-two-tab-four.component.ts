2a257230084149c3d74c561bed41767d6b10fdb5
Nutti Saelor <leelorz6@gmail.com>
2022-09-19 23:32:22 +0700

    feature: populate data for options in foreign request form

--- libs/self-service/form/src/lib/foreign-step-two-tab-four/foreign-step-two-tab-four.component.ts
+++ libs/self-service/form/src/lib/foreign-step-two-tab-four/foreign-step-two-tab-four.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,6 +9,8 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./foreign-step-two-tab-four.component.scss'],
 })
 export class ForeignStepTwoTabFourComponent implements OnInit {
+  @Input() countryList: any[] = [];
+
   constructor() {}
 
   ngOnInit(): void {}
