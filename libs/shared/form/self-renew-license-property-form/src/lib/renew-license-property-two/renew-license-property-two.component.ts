12a5d63d0425d67ea537a391e80305e3c05fa44f
Nutti Saelor <leelorz6@gmail.com>
2022-09-21 22:38:19 +0700

    feature: add request for renew-request school manager

--- libs/shared/form/self-renew-license-property-form/src/lib/renew-license-property-two/renew-license-property-two.component.ts
+++ libs/shared/form/self-renew-license-property-form/src/lib/renew-license-property-two/renew-license-property-two.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { KspFormBaseComponent } from '@ksp/shared/interface';
+import { providerFactory } from '@ksp/shared/utility';
 
 @Component({
   selector: 'ksp-renew-license-property-two',
   templateUrl: './renew-license-property-two.component.html',
   styleUrls: ['./renew-license-property-two.component.scss'],
+  providers: providerFactory(RenewLicensePropertyTwoComponent),
 })
-export class RenewLicensePropertyTwoComponent implements OnInit {
-  constructor() {}
+export class RenewLicensePropertyTwoComponent extends KspFormBaseComponent {
+  override form = this.fb.group({
+    degree: [],
+    managingDegree: [],
+  });
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder) {
+    super();
+    this.subscriptions.push(
+      // any time the inner form changes update the parent of any change
+      this.form?.valueChanges.subscribe((value) => {
+        this.onChange(value);
+        this.onTouched();
+      })
+    );
+  }
 }
