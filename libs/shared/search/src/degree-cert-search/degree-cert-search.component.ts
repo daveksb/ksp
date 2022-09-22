480fac1bd0b1ddc1a07f19a98b2c62e3a1db676a
nuttapon-nk <nuttapon.krairach@gmail.com>
2022-09-22 17:08:28 +0700

    feature: Add KspFormBaseComponent

--- libs/shared/search/src/degree-cert-search/degree-cert-search.component.ts
+++ libs/shared/search/src/degree-cert-search/degree-cert-search.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { KspFormBaseComponent } from '@ksp/shared/interface';
+import { providerFactory } from '@ksp/shared/utility';
 import { UntilDestroy } from '@ngneat/until-destroy';
 
 @UntilDestroy()
@@ -10,9 +12,10 @@ import { UntilDestroy } from '@ngneat/until-destroy';
   styleUrls: ['./degree-cert-search.component.scss'],
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
+  providers: providerFactory(DegreeCertSearchComponent),
 })
-export class DegreeCertSearchComponent {
-  form = this.fb.group({
+export class DegreeCertSearchComponent extends KspFormBaseComponent {
+  override form = this.fb.group({
     licenseNumber: [],
     date: [],
     degreeName: [],
@@ -27,5 +30,14 @@ export class DegreeCertSearchComponent {
   @Output() searched = new EventEmitter<boolean>();
   @Output() cleared = new EventEmitter<boolean>();
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    super();
+    this.subscriptions.push(
+      // any time the inner form changes update the parent of any change
+      this.form?.valueChanges.subscribe((value) => {
+        this.onChange(value);
+        this.onTouched();
+      })
+    );
+  }
 }
