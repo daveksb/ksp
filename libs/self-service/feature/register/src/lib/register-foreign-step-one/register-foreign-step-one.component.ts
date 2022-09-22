3ebcc86d14bb84e29d54cf3d84703a8f432cbcd4
Your Name <you@example.com>
2022-09-23 00:12:29 +0700

    update save foreight

--- libs/self-service/feature/register/src/lib/register-foreign-step-one/register-foreign-step-one.component.ts
+++ libs/self-service/feature/register/src/lib/register-foreign-step-one/register-foreign-step-one.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import localForage from 'localforage';
 @Component({
   templateUrl: './register-foreign-step-one.component.html',
   styleUrls: ['./register-foreign-step-one.component.scss'],
 })
 export class RegisterForeignStepOneComponent {
   constructor(private router: Router, private fb: FormBuilder) {}
+  form = this.fb.group({
+    prefixen: [],
+    firstnameen: [],
+    middlenameen: [],
+    lastnameen: [],
+    birthdate: [],
+    country: [],
+    nationality: [],
+    phone: [],
+    email: [],
+  });
 
   next() {
-    this.router.navigate(['/register', 'en-step-2']);
+    localForage.getItem('registerForeign').then((res: any) => {
+      const data = { ...res, ...this.form.value };
+      localForage.setItem('registerForeignr', data);
+      this.router.navigate(['/register', 'en-step-2']);
+    });
   }
 
   loginPage() {
