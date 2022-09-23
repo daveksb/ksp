a9c18f31bddd4995a8fcad1a6e45806b53ac1ad3
nuttapon-nk <nuttapon.krairach@gmail.com>
2022-09-23 23:34:16 +0700

    feature: Add  uploadComplete

--- libs/shared/form/others/src/lib/form-attachment/form-attachment.component.ts
+++ libs/shared/form/others/src/lib/form-attachment/form-attachment.component.ts
@@ -20,6 +20,7 @@ export class FormAttachmentComponent {
   @Input() uniqueTimestamp: string | null = null;
   @Input() requestType: number | null = null;
   @Output() downloadClick = new EventEmitter<any>();
+  @Output() uploadComplete = new EventEmitter<any>();
 
   constructor(
     public dialog: MatDialog,
@@ -61,5 +62,6 @@ export class FormAttachmentComponent {
     const { fileId, fileName } = file;
     group.fileId = fileId;
     group.fileName = fileName;
+    this.uploadComplete.emit(this.groups);
   }
 }
