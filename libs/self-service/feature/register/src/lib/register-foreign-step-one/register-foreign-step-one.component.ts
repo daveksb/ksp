41316bd8d45076f298619b19a564f630df7162ae
Kosol Komenek <kosolkomenek@Kosols-MacBook-Air.local>
2022-09-24 14:41:52 +0700

    update async pipe

--- libs/self-service/feature/register/src/lib/register-foreign-step-one/register-foreign-step-one.component.ts
+++ libs/self-service/feature/register/src/lib/register-foreign-step-one/register-foreign-step-one.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AddressService, GeneralInfoService } from '@ksp/shared/service';
 import localForage from 'localforage';
+import { Observable } from 'rxjs';
 @Component({
   templateUrl: './register-foreign-step-one.component.html',
   styleUrls: ['./register-foreign-step-one.component.scss'],
 })
-export class RegisterForeignStepOneComponent {
-  constructor(private router: Router, private fb: FormBuilder) {}
+export class RegisterForeignStepOneComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private generalInfoService: GeneralInfoService,
+    private addressService: AddressService
+  ) {}
+  prefixList$!: Observable<any>;
+  countries$!: Observable<any>;
   form = this.fb.group({
     prefixen: [],
     firstnameen: [],
@@ -19,7 +28,10 @@ export class RegisterForeignStepOneComponent {
     phone: [],
     email: [],
   });
-
+  ngOnInit() {
+    this.prefixList$ = this.generalInfoService.getPrefix();
+    this.countries$ = this.addressService.getCountry();
+  }
   next() {
     localForage.getItem('registerForeign').then((res: any) => {
       const data = { ...res, ...this.form.value };
--- libs/self-service/feature/register/src/lib/register-foreign-step-two/register-foreign-step-two.component.ts
+++ libs/self-service/feature/register/src/lib/register-foreign-step-two/register-foreign-step-two.component.ts
@@ -1,20 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { VerifyOtpForeignDialogComponent } from '@ksp/self-service/dialog';
+import { GeneralInfoService } from '@ksp/shared/service';
 import localForage from 'localforage';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'self-service-register-foreign-step-two',
   templateUrl: './register-foreign-step-two.component.html',
   styleUrls: ['./register-foreign-step-two.component.scss'],
 })
-export class RegisterForeignStepTwoComponent {
+export class RegisterForeignStepTwoComponent implements OnInit {
   constructor(
     public dialog: MatDialog,
     private router: Router, //private fb: FormBuilder,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private generalInfoService: GeneralInfoService
   ) {}
+  visaClassList$!: Observable<any>;
+  visaTypeList$!: Observable<any>;
   form = this.fb.group({
     idcardno: [],
     passportno: [],
@@ -24,6 +29,11 @@ export class RegisterForeignStepTwoComponent {
     visatype: [],
     visaenddate: [],
   });
+
+  ngOnInit() {
+    this.visaClassList$ = this.generalInfoService.getVisaClass();
+    this.visaTypeList$ = this.generalInfoService.getVisaType();
+  }
   openDialog() {
     const dialogRef = this.dialog.open(VerifyOtpForeignDialogComponent, {
       width: '600px',
