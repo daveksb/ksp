9cacddcd115e81686eadf0482c9fe814ccdefedb
daveksb <flying.geez@gmail.com>
2022-09-24 17:09:17 +0700

    fiix menu

--- libs/school-service/feature/container-page/src/lib/school-container-page/school-container-page.component.ts
+++ libs/school-service/feature/container-page/src/lib/school-container-page/school-container-page.component.ts
@@ -19,70 +19,70 @@ export const menu: MenuConfig[] = [
   {
     icon: 'assets/images/icon-sidenav/home.svg ',
     label: 'หน้าแรก',
-    path: 'temp-license/list',
+    path: '/temp-license/list',
   },
   {
     icon: 'assets/images/icon-sidenav/paper.svg',
     label: 'ยื่นใบคำขอ',
-    path: '',
+    path: '/',
     subMenuName: 'license',
     isExpanded: false,
     subMenu: [
       {
         label: 'ขอใบอนุญาตประกอบวิชาชีพโดยไม่มีใบอนุญาตประกอบวิชาชีพ (ชาวไทย)',
-        path: 'temp-license/request',
+        path: '/temp-license/request',
         params: { subtype: SchoolRequestSubType.ครู },
       },
       {
         label:
           'ขออนุญาตประกอบวิชาชีพโดยไม่มีใบอนุญาตประกอบวิชาชีพ (ผู้บริหารสถานศึกษา)',
-        path: 'temp-license/request',
+        path: '/temp-license/request',
         params: { subtype: SchoolRequestSubType.ผู้บริหารสถานศึกษา },
       },
       {
         label:
           'ขออนุญาตประกอบวิชาชีพโดยไม่มีใบอนุญาตประกอบวิชาชีพ (ชาวต่างชาติ)',
-        path: 'temp-license/request',
+        path: '/temp-license/request',
         params: { subtype: SchoolRequestSubType.อื่นๆ },
       },
       {
         label: 'ขอสร้างเลขประจำตัวคุรุสภาสำหรับครูชาวต่างชาติ',
-        path: 'foreign-teacher/id-request',
+        path: '/foreign-teacher/id-request',
       },
       {
         label: 'ขอหนังสือรับรองคุณวุฒิ',
-        path: 'qualification-approve/detail',
+        path: '/qualification-approve/detail',
       },
       {
         label:
           'ขอรับรางวัลหนึ่งโรงเรียนหนึ่งนวัตกรรม (One School One Innovation : OSOI)',
-        path: 'request-reward/detail',
+        path: '/request-reward/detail',
       },
     ],
   },
   {
     icon: 'assets/images/icon-sidenav/display.svg',
     label: 'ทะเบียนบุคลากรภายในหน่วยงาน',
-    path: 'staff-management/list',
+    path: '/staff-management/list',
   },
   {
     icon: 'assets/images/icon-sidenav/display.svg',
     label: 'ทะเบียนหนังสืออนุญาตประกอบวิชาชีพโดยไม่มีใบอนุญาต',
-    path: '',
+    path: '/',
   },
   {
     icon: 'assets/images/icon-sidenav/display.svg',
     label: 'กิจกกรมพัฒนาตนเอง (หนังสืออนุญาตประกอบวิชาชีพโดยไม่มีใบอนุญาต)',
-    path: 'activity',
+    path: '/activity',
   },
   {
     icon: 'assets/images/icon-sidenav/file-earmark-text-fill.svg',
     label: 'รายงาน',
-    path: '',
+    path: '/',
   },
   {
     icon: 'assets/images/icon-sidenav/gear-fill.svg',
     label: 'รายชื่อเจ้าหน้าที่ใช้งาน',
-    path: '',
+    path: '/',
   },
 ];
