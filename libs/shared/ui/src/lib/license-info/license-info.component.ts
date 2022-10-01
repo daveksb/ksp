d7bcdce919a5b79ec13eb2b3700aae2789910ba5
daveksb <flying.geez@gmail.com>
2022-10-01 15:46:15 +0700

    license info

--- libs/shared/ui/src/lib/license-info/license-info.component.ts
+++ libs/shared/ui/src/lib/license-info/license-info.component.ts
@@ -15,9 +15,19 @@ export class LicenseInfoComponent {
   @Input() subTitle1 = 'วันเดือนปีที่ออก';
   @Input() subTitle2 = 'วันเดือนปีที่หมดอายุ';
 
+  @Input() licenseNo: string | null = '';
+  @Input() careerType: string | null = '';
+  @Input() idCardNo: string | null = '';
+  @Input() licenseType: string | null = '';
+  @Input() thaiName: string | null = '';
+  @Input() enName: string | null = '';
+  @Input() birthDate: string | null = '';
+  @Input() licenseStartDate: string | null = '';
+  @Input() licenseEndDate: string | null = '';
+
   constructor(private router: Router) {}
 
   select() {
-    this.router.navigate(['/', 'staff-management', 'staff-person-info']);
+    this.router.navigate(['/staff-management', 'staff-person-info']);
   }
 }
