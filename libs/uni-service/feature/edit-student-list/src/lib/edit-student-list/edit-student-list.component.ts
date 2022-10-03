ab7f5f16c6e0c57c9935f70e33ca8f38e9d84ab2
พิสิษฐ์ เสนาวงค์ <mrsacksiiz_amphersand@Macintosh.local>
2022-10-03 22:47:42 +0700

    fix edit list form

--- libs/uni-service/feature/edit-student-list/src/lib/edit-student-list/edit-student-list.component.ts
+++ libs/uni-service/feature/edit-student-list/src/lib/edit-student-list/edit-student-list.component.ts
@@ -35,7 +35,7 @@ export class EditStudentListComponent implements OnInit {
     coursemajor: [],
     plancalendaryear: [],
     requeststatus: [],
-    idcard: [],
+    idcardno: [],
     name: [],
     requestdatefrom: [],
     requestdateto: []
