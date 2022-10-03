0830a8e0bd7d784130f2aee1e06a0ec32a89457c
nuttapon.nk <nuttapon.krairach@gmail.com>
2022-10-03 18:53:50 +0700

    feature: add unirequestdegreecertselectunidegreecertid

--- libs/uni-service/dialog/src/lib/history-request-dialog/history-request-dialog.component.ts
+++ libs/uni-service/dialog/src/lib/history-request-dialog/history-request-dialog.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatDialogModule } from '@angular/material/dialog';
-
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { stringToThaiDate } from '@ksp/shared/utility';
 @Component({
   selector: 'uni-service-history-request-dialog',
   standalone: true,
@@ -10,7 +10,25 @@ import { MatDialogModule } from '@angular/material/dialog';
   styleUrls: ['./history-request-dialog.component.scss'],
 })
 export class HistoryRequestDialogComponent implements OnInit {
-  constructor() {}
+  constructor(@Inject(MAT_DIALOG_DATA) public dataSource: any) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.dataSource);
+  }
+  toDateTh(date: any) {
+    try {
+      return stringToThaiDate(date);
+    } catch (error) {
+      return "-"
+    }
+   
+  }
+  otStatus(data: any) {
+    const status: any = {
+      '1': 'ยื่น',
+      '2': 'รับข้อมูล',
+    };
+
+    return data ? status[data] : '-';
+  }
 }
