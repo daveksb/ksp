ef98abfc5d554223fa99d087ba12a5c697bb2c7f
dew4356 <dulyanut.nitmai@kkumail.com>
2022-10-08 11:59:26 +0700

    edit

--- libs/e-service/e-license/qualification-approve/src/lib/e-service-e-license-qualification-approve.module.ts
+++ libs/e-service/e-license/qualification-approve/src/lib/e-service-e-license-qualification-approve.module.ts
@@ -4,7 +4,18 @@ import { QualificationApproveListComponent } from './qualification-approve-list/
 import { QualificationApproveDetailComponent } from './qualification-approve-detail/qualification-approve-detail.component';
 import { Route, RouterModule } from '@angular/router';
 import { EServiceContainerPageComponent } from '@ksp/e-service/feature/container-page';
-import { TopNavComponent } from '@ksp/shared/menu';
+import { BottomNavComponent, TopNavComponent } from '@ksp/shared/menu';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RequestHeaderInfoComponent } from '@ksp/shared/ui';
+import { SharedFormOthersModule } from '@ksp/shared/form/others';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  EServiceRequestSearchComponent,
+  RequestSearchComponent,
+} from '@ksp/shared/search';
+import { MatTableModule } from '@angular/material/table';
+import { LicenseCheckComponent } from '@ksp/e-service/ui/license-check';
 
 export const routes: Route[] = [
   {
@@ -29,7 +40,20 @@ export const routes: Route[] = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), TopNavComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    TopNavComponent,
+    MatTabsModule,
+    RequestHeaderInfoComponent,
+    SharedFormOthersModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    EServiceRequestSearchComponent,
+    LicenseCheckComponent,
+    BottomNavComponent,
+  ],
   declarations: [
     QualificationApproveListComponent,
     QualificationApproveDetailComponent,
--- libs/e-service/e-license/qualification-approve/src/lib/qualification-approve-list/qualification-approve-list.component.ts
+++ libs/e-service/e-license/qualification-approve/src/lib/qualification-approve-list/qualification-approve-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,11 +9,58 @@ import { Router } from '@angular/router';
   styleUrls: ['./qualification-approve-list.component.scss'],
 })
 export class QualificationApproveListComponent implements OnInit {
-  constructor(private router: Router) {}
+  form = this.fb.group({
+    foreignSearch: [],
+  });
+
+  displayedColumns: string[] = column;
+  dataSource = new MatTableDataSource<any>();
+
+  constructor(private router: Router, private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  search() {
+    this.dataSource.data = data;
+  }
+
+  clear() {
+    this.dataSource.data = [];
+  }
 
   next() {
     this.router.navigate(['/qualification', 'detail']);
   }
 }
+
+export const data = [
+  {
+    id: 1,
+    view: 'xxx',
+    requestno: 'xxx',
+    idcardno: 'xxx',
+    name: 'xxx',
+    profession: 'xxx',
+    artification: 'xxx',
+    schoolname: 'xxx',
+    requeststatus: 'xxx',
+    status: 'xxx',
+    updatedate: 'xxx',
+    requestdate: 'xxx',
+  },
+];
+
+export const column = [
+  'id',
+  'view',
+  'requestno',
+  'idcardno',
+  'name',
+  'profession',
+  'artification',
+  'schoolname',
+  'requeststatus',
+  'status',
+  'updatedate',
+  'requestdate',
+];
