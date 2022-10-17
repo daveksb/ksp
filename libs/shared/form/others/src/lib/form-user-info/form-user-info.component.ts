1fe64781e2be6f970c5e74eb3f1f5a3f9c650fa6
daveksb <flying.geez@gmail.com>
2022-10-17 12:35:52 +0700

    self validate

--- libs/shared/form/others/src/lib/form-user-info/form-user-info.component.ts
+++ libs/shared/form/others/src/lib/form-user-info/form-user-info.component.ts
@@ -51,7 +51,6 @@ export class FormUserInfoComponent
   @Input() isSelfService = false;
   @Input() isAddStaff = false;
   @Input() requiredIdCardNo = true;
-
   public _displayMode = UserInfoFormType.thai;
   @Input()
   set displayMode(mode: number) {
@@ -86,7 +85,7 @@ export class FormUserInfoComponent
     // คนไทยไม่ต้อง validate field เหล่านี้
     if (mode === UserInfoFormType.thai) {
       //console.log('aa = ');
-      //this.form.controls.passportno.clearValidators();
+      this.form.controls.passportno.clearValidators();
       this.form.controls.kuruspano.clearValidators();
       this.form.controls.passportstartdate.clearValidators();
       this.form.controls.passportenddate.clearValidators();
@@ -105,6 +104,8 @@ export class FormUserInfoComponent
   }
 
   ngOnInit(): void {
+    this.checkValidators(this._displayMode);
+
     this.form.controls.idcardno.valueChanges
       .pipe(debounceTime(200), distinctUntilChanged())
       .subscribe((res) => {
