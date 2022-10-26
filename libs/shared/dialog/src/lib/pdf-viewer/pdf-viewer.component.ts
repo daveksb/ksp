42e58556aa74d3b12c6205f90da04a62c0732e74
Kosol Komenek <kosolkomenek@Kosols-MacBook-Air-2.local>
2022-10-26 23:55:10 +0700

    update feature/pdfrender

--- libs/shared/dialog/src/lib/pdf-viewer/pdf-viewer.component.ts
+++ libs/shared/dialog/src/lib/pdf-viewer/pdf-viewer.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { LicenseCheckComponent } from '@ksp/e-service/ui/license-check';
-import { PDFDocument, rgb } from 'pdf-lib';
+import { PDFDocument, PDFFont, PDFPage } from 'pdf-lib';
 import fontkit from '@pdf-lib/fontkit';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { IKspInput, kspPdfMapping } from '@ksp/shared/interface';
+import _ from 'lodash';
 
 @Component({
   selector: 'ksp-pdf-viewer-preview',
@@ -20,32 +22,47 @@ import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 })
 export class PdfViewerComponent implements OnInit {
   pdfBytes: any;
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      pdfType: number;
+      fontSrc: string;
+      input: any;
+    }
+  ) {}
   ngOnInit(): void {
-    this.modifyPdf();
+    if (this.data.pdfType) this.modifyPdf();
   }
   async modifyPdf() {
-    const url = 'assets/pdf/example.pdf';
-    const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
-
+    const pdf = kspPdfMapping.find(
+      (item) => (item.pdfType = this.data.pdfType)
+    );
+    if (!pdf) return;
+    const existingPdfBytes = await fetch(pdf?.pdfSrc).then((res) =>
+      res.arrayBuffer()
+    );
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
-    // const fontBytes = 'http://www.fontsaddict.com/fontface/thalia-normal.ttf';
-    const urlF = 'assets/font/ksp/ksp-regular.ttf';
-    const fontBytes = await fetch(urlF).then((res) => res.arrayBuffer());
+    const fontBytes = await fetch(
+      this.data.fontSrc || 'assets/font/ksp/ksp-regular.ttf'
+    ).then((res) => res.arrayBuffer());
     pdfDoc.registerFontkit(fontkit);
     const customFont = await pdfDoc.embedFont(fontBytes, { subset: false });
 
     const pages = pdfDoc.getPages();
-    const firstPage = pages[0];
-    const { width, height } = firstPage.getSize();
-    firstPage.drawText('เฮงงงงงงงงงงงงง', {
-      x: 165,
-      y: 675,
-      size: 25,
-      font: customFont,
-      color: rgb(0.95, 0.1, 0.1),
-      // rotate: degrees(0),
-    });
-
+    this.createPdf(pdf.input, pages, customFont);
     this.pdfBytes = await pdfDoc.save();
+  }
+  createPdf(input: IKspInput[][], pages: PDFPage[], customFont: PDFFont) {
+    for (const index in pages) {
+      if (!input[index]) return;
+      for (const param of input[index]) {
+        const dataRender = this.data.input[param.key] || '';
+        pages[index].drawText(dataRender, {
+          ...param.options,
+          font: customFont,
+        });
+        pages[index].drawSvgPath('M18 7L9.42857 17L6 13', { x: 45, y: 328 });
+      }
+    }
   }
 }
