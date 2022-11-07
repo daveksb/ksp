cfa4311a2ccdeebf5b420aa31fb07b81b2aa3a15
daveksb <flying.geez@gmail.com>
2022-11-07 08:31:38 +0700

    kmv 1

--- libs/e-service/e-license/request-license/src/lib/request-license-approve-license-table/request-license-approve-license-table.component.ts
+++ libs/e-service/e-license/request-license/src/lib/request-license-approve-license-table/request-license-approve-license-table.component.ts
@@ -7,44 +7,74 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./request-license-approve-license-table.component.scss'],
 })
 export class RequestLicenseApproveLicenseTableComponent implements OnInit {
+  @Input() isKmv = false;
   @Input() data = [
     {
       order: 1,
       licenseType: 'ครู',
       count: 0,
+      approve: 10,
+      unApprove: 0,
+      urgent: 1,
     },
     {
       order: 2,
       licenseType: 'ครูชาวต่างชาติ',
       count: 0,
+      approve: 0,
+      unApprove: 0,
+      urgent: 0,
     },
     {
       order: 3,
       licenseType: 'KSP Bundit',
       count: 1,
+      approve: 67,
+      unApprove: 0,
+      urgent: 0,
     },
     {
       order: 4,
       licenseType: 'ผู้บริหารสถานศึกษา',
       count: 1,
+      approve: 22,
+      unApprove: 0,
+      urgent: 1,
     },
     {
       order: 5,
       licenseType: 'ผู้บริหารการศึกษา',
       count: 0,
+      approve: 0,
+      unApprove: 1,
+      urgent: 0,
     },
     {
       order: 6,
       licenseType: 'ศึกษานิเทศก์',
       count: 0,
+      approve: 1,
+      unApprove: 0,
+      urgent: 0,
     },
   ];
-  displayedColumns = ['order', 'licenseType', 'count'];
+  displayedColumns: string[] = [];
   dataSource = new MatTableDataSource<any>();
 
   constructor() {}
 
   ngOnInit(): void {
     this.dataSource.data = this.data;
+    this.isKmv
+      ? (this.displayedColumns = [
+          'order',
+          'licenseType',
+          'count',
+          'viewDetail',
+          'approve',
+          'unApprove',
+          'urgent',
+        ])
+      : (this.displayedColumns = ['order', 'licenseType', 'count']);
   }
 }
