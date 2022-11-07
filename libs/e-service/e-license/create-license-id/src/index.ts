97ca99a914f5e8ab5d7f6cb4bee1c8aaf8290f64
dew4356 <dulyanut.nitmai@kkumail.com>
2022-11-07 16:24:00 +0700

    create license id detail

---
+++ libs/e-service/e-license/create-license-id/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/e-service-e-license-create-license-id.module';
+
+export * from './lib/create-license-id-detail/create-license-id-detail.component';
+
+export * from './lib/create-license-id-list/create-license-id-list.component';
