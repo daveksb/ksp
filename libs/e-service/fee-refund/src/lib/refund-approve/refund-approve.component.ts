c4540b45bb0342925655e19a7400d6695959b08a
Nutti Saelor <leelorz6@gmail.com>
2022-11-18 23:46:23 +0700

    feature: fix fee refund components

--- libs/e-service/fee-refund/src/lib/refund-approve/refund-approve.component.ts
+++ libs/e-service/fee-refund/src/lib/refund-approve/refund-approve.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ERewardConfirmFormBaseComponent } from '@ksp/self-service/form';
+import { ERequestService } from '@ksp/shared/service';
 
 @Component({
   selector: 'ksp-refund-approve',
   templateUrl: './refund-approve.component.html',
   styleUrls: ['./refund-approve.component.scss'],
 })
-export class RefundApproveComponent implements OnInit {
-  constructor(private router: Router) {}
-
-  back() {
-    this.router.navigate(['/', 'refund', 'detail']);
+export class RefundApproveComponent extends ERewardConfirmFormBaseComponent {
+  constructor(
+    fb: FormBuilder,
+    route: ActivatedRoute,
+    private router: Router,
+    dialog: MatDialog,
+    eRequestService: ERequestService
+  ) {
+    super(fb, route, dialog, eRequestService);
   }
 
-  cancel() {
+  prevPage() {
+    this.router.navigate(['/', 'refund', 'detail', this.requestId]);
+  }
+
+  navigateBack() {
     this.router.navigate(['/', 'refund', 'list']);
   }
-
-  ngOnInit(): void {}
 }
