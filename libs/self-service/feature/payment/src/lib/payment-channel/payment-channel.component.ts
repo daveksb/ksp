89758c4ecd016106199dfe29d9b01811a0901654
dew4356 <dulyanut.nitmai@kkumail.com>
2022-12-03 00:53:00 +0700

    edit self side nav and payment

--- libs/self-service/feature/payment/src/lib/payment-channel/payment-channel.component.ts
+++ libs/self-service/feature/payment/src/lib/payment-channel/payment-channel.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CompleteDialogComponent } from '@ksp/shared/dialog';
+import { thaiDate } from '@ksp/shared/utility';
+import localForage from 'localforage';
 
 @Component({
   templateUrl: './payment-channel.component.html',
   styleUrls: ['./payment-channel.component.css'],
 })
-export class PaymentChannelComponent {
+export class PaymentChannelComponent implements OnInit {
+  requestno!: string;
+  requestdate!: string;
+
   constructor(public router: Router) {}
+
+  ngOnInit(): void {
+    localForage.getItem('requestno').then((res: any) => {
+      //console.log('xxx = ', res);
+      this.requestno = res.requestno;
+    });
+  }
 
   promptpay(type: any) {
     this.router.navigate(['/', 'license', 'payment-promptpay', type]);
