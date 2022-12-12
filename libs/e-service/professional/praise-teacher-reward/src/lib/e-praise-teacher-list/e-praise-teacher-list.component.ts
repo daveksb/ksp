fdd2da4d6ef19d40343939588d120e9538588876
Nutti Saelor <leelorz6@gmail.com>
2022-12-12 23:29:16 +0700

    feature: add teacher council reject component

--- libs/e-service/professional/praise-teacher-reward/src/lib/e-praise-teacher-list/e-praise-teacher-list.component.ts
+++ libs/e-service/professional/praise-teacher-reward/src/lib/e-praise-teacher-list/e-praise-teacher-list.component.ts
@@ -74,6 +74,10 @@ export class EPraiseTeacherListComponent implements OnInit, AfterViewInit {
   view(id: number) {
     this.router.navigate(['/praise-teacher', 'detail', id]);
   }
+
+  reject(id: number) {
+    this.router.navigate(['/praise-teacher', 'reject', id]);
+  }
 }
 
 export const column = [
