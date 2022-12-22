842c0e0be8ece70d85d59d12a40e1e3fc62cc065
dew4356 <dulyanut.nitmai@kkumail.com>
2022-12-23 00:36:50 +0700

    edit

--- apps/e-service/src/app/app.menu.config.ts
+++ apps/e-service/src/app/app.menu.config.ts
@@ -247,11 +247,6 @@ export const eLicenseMenu: MenuConfig[] = [
     ],
   }, */
   {
-    icon: 'assets/images/icon-sidenav/paper.svg',
-    label: 'ใบคำขอผู้ใช้งานระบบบริการหน่วยงานทางการศึกษา (School Service)',
-    path: '/school/new-user',
-  },
-  {
     icon: 'assets/images/icon-sidenav/card.svg',
     label: 'ทะเบียนข้อมูล',
     path: '/xxx',
@@ -277,6 +272,11 @@ export const eLicenseMenu: MenuConfig[] = [
     icon: 'assets/images/icon-sidenav/card.svg',
     label: 'รายงาน',
     path: '/xxx',
+  },
+  {
+    icon: 'assets/images/icon-sidenav/paper.svg',
+    label: 'ใบคำขอผู้ใช้งานระบบบริการหน่วยงานทางการศึกษา (School Service)',
+    path: '/school/new-user',
   },
   {
     icon: 'assets/images/icon-sidenav/people.svg',
@@ -397,27 +397,6 @@ export const standardMenu: MenuConfig[] = [
     ],
   }, */
   {
-    icon: 'assets/images/icon-sidenav/card.svg',
-    label: 'ใบคำขอรับรองคุณวุฒิทางการศึกษา',
-    path: '',
-    isExpanded: false,
-    subMenuName: 'qualification',
-    subMenu: [
-      {
-        path: '/qualification-approve/list',
-        label: 'รายการใบคำรับรองคุณวุฒิทางการศึกษา',
-      },
-      {
-        path: '/cc',
-        label: 'บัญชีรายชื่อและพิมพ์หนังสือคุณวุฒิทางการศึกษา',
-      },
-      {
-        path: '/bb',
-        label: 'พิจารณารับรองคุณวุฒิทางการศึกษา',
-      },
-    ],
-  },
-  {
     icon: 'assets/images/icon-sidenav/paper.svg',
     label: 'ใบคำขอเทียบเคียงความรู้',
     path: '',
@@ -456,6 +435,27 @@ export const standardMenu: MenuConfig[] = [
       {
         path: '/xxx',
         label: 'พิจารณารับรองรับรองความรู้',
+      },
+    ],
+  },
+  {
+    icon: 'assets/images/icon-sidenav/card.svg',
+    label: 'ใบคำขอรับรองคุณวุฒิทางการศึกษา',
+    path: '',
+    isExpanded: false,
+    subMenuName: 'qualification',
+    subMenu: [
+      {
+        path: '/qualification-approve/list',
+        label: 'รายการใบคำรับรองคุณวุฒิทางการศึกษา',
+      },
+      {
+        path: '/cc',
+        label: 'บัญชีรายชื่อและพิมพ์หนังสือคุณวุฒิทางการศึกษา',
+      },
+      {
+        path: '/bb',
+        label: 'พิจารณารับรองคุณวุฒิทางการศึกษา',
       },
     ],
   },
