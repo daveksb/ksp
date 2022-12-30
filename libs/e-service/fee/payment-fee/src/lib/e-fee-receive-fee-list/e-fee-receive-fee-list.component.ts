a330c8dc95ea792f7a6d16491923f20656d3d8d9
dew4356 <dulyanut.nitmai@kkumail.com>
2022-12-30 17:40:02 +0700

    es fee ui 2

--- libs/e-service/fee/payment-fee/src/lib/e-fee-receive-fee-list/e-fee-receive-fee-list.component.ts
+++ libs/e-service/fee/payment-fee/src/lib/e-fee-receive-fee-list/e-fee-receive-fee-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { LoaderService } from '@ksp/shared/service';
 
 @Component({
   selector: 'ksp-e-fee-receive-fee-list',
@@ -6,7 +11,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./e-fee-receive-fee-list.component.scss'],
 })
 export class EFeeReceiveFeeListComponent implements OnInit {
-  constructor() {}
+  displayedColumns: string[] = column;
+  dataSource = new MatTableDataSource<refundConfirmInfo>();
 
-  ngOnInit(): void {}
+  form = this.fb.group({
+    licensetype: [],
+    receiptno: [],
+    licenseno: [],
+    idcardno: [],
+    refid: [],
+    paymentchannel: [],
+    paymentstatus: [],
+    paymentdate: [],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private loaderService: LoaderService,
+    private router: Router,
+    public dialog: MatDialog
+  ) {}
+
+  search() {
+    this.dataSource.data = data;
+  }
+
+  clear() {
+    this.dataSource.data = [];
+  }
+
+  cancel() {
+    this.router.navigate(['/', 'payment-fee', 'detail']);
+  }
+
+  ngOnInit(): void {
+    this.search();
+  }
 }
+
+interface refundConfirmInfo {
+  order: number;
+  receiptno: string;
+  refid: string;
+  idcardno: string;
+  name: string;
+  feetype: string;
+  amount: string;
+}
+
+const data: refundConfirmInfo[] = [
+  {
+    order: 1,
+    receiptno: 'receiptno',
+    refid: 'refid',
+    idcardno: 'idcardno',
+    name: 'name',
+    feetype: 'feetype',
+    amount: 'amount',
+  },
+];
+
+const column = [
+  'order',
+  'name',
+  'idcardno',
+  'refid',
+  'receiptno',
+  'amount',
+  'feetype',
+];
