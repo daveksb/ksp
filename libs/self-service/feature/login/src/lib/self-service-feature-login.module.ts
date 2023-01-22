437248548bdfbf8ee4daaa58b1bc80af97c542b5
dew4356 <dulyanut.nitmai@kkumail.com>
2023-01-23 01:15:17 +0700

    self foreign register fix

--- libs/self-service/feature/login/src/lib/self-service-feature-login.module.ts
+++ libs/self-service/feature/login/src/lib/self-service-feature-login.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SelfServiceThaiLoginComponent } from './self-service-thai-login/self-service-thai-login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UniFormBadgeComponent } from '@ksp/shared/ui';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, UniFormBadgeComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    UniFormBadgeComponent,
+    MatButtonToggleModule,
+  ],
   declarations: [SelfServiceThaiLoginComponent],
 })
 export class SelfServiceFeatureLoginModule {}
