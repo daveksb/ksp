363dca80fb3d433034b9a29ff4d9c807a9ac00ec
daveksb <flying.geez@gmail.com>
2023-01-23 17:50:24 +0700

    search kspno

--- libs/self-service/feature/register/src/lib/register-foreign/register-foreign.component.ts
+++ libs/self-service/feature/register/src/lib/register-foreign/register-foreign.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { SelfRequestService } from '@ksp/shared/service';
 import localForage from 'localforage';
 @Component({
   //selector: 'ksp-register-foreign',
@@ -8,16 +9,33 @@ import localForage from 'localforage';
   styleUrls: ['./register-foreign.component.scss'],
 })
 export class RegisterForeignComponent {
+  searchSubmit = false;
   form = this.fb.group({
     kuruspano: [null, [Validators.required]],
     passportno: [null, [Validators.required]],
   });
 
-  constructor(private router: Router, private fb: FormBuilder) {}
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private requestService: SelfRequestService
+  ) {}
 
   submit() {
-    localForage.setItem('registerForeigner', this.form.value);
-    this.router.navigate(['/register', 'en-step-1']);
+    const kspNo = this.form.value.kuruspano;
+    if (kspNo) {
+      this.requestService.searchKuruspano(kspNo).subscribe((res) => {
+        if (res && res.returncode && res.returncode === '98') {
+          //not found
+          this.searchSubmit = true;
+          console.log('not found !!');
+        } else {
+          console.log('res xx = ', res);
+          localForage.setItem('registerForeigner', res);
+          this.router.navigate(['/register', 'en-step-1']);
+        }
+      });
+    }
   }
 
   loginPage() {
