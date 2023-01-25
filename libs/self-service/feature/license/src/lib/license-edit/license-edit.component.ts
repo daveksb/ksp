a171af8475c7fdc1dd150381e542c34015d4be86
daveksb <flying.geez@gmail.com>
2023-01-25 12:01:31 +0700

    get current license

--- libs/self-service/feature/license/src/lib/license-edit/license-edit.component.ts
+++ libs/self-service/feature/license/src/lib/license-edit/license-edit.component.ts
@@ -17,9 +17,15 @@ import {
   getCookie,
   parseJson,
   replaceEmptyWithNull,
+  thaiDate,
   toLowercaseProp,
 } from '@ksp/shared/utility';
-import { FileGroup, SelfRequest } from '@ksp/shared/interface';
+import {
+  FileGroup,
+  Prefix,
+  SelfLicense,
+  SelfRequest,
+} from '@ksp/shared/interface';
 import {
   SelfServiceRequestForType,
   SelfServiceRequestSubType,
@@ -28,39 +34,6 @@ import {
 import * as _ from 'lodash';
 import { v4 as uuidv4 } from 'uuid';
 
-/* const FILE_LIST = [
-  {
-    name: 'สำเนาหนังสือสำคัญการเปลี่ยนชื่อ/ชื่อสกุล/เปลี่ยนหรือเพิ่มคำนำหน้าชื่อ',
-    fileid: '',
-    filename: '',
-  },
-  {
-    name: 'สำเนาหลักฐานการสมรส หรือการสิ้นสุดการสมรส (ถ้ามี)',
-    fileid: '',
-    filename: '',
-  },
-  {
-    name: 'สำเนาหนังสือรับรองการใช้คำหน้านามหญิง (ถ้ามี)',
-    fileid: '',
-    filename: '',
-  },
-]; */
-
-const FILE_LIST: FileGroup[] = [
-  {
-    name: '1. สำเนาหนังสือสำคัญการเปลี่ยนชื่อ/ชื่อสกุล/เปลี่ยนหรือเพิ่มคำนำหน้าชื่อ',
-    files: [],
-  },
-  {
-    name: '2. สำเนาหลักฐานการสมรส หรือการสิ้นสุดการสมรส (ถ้ามี)',
-    files: [],
-  },
-  {
-    name: '3. สำเนาหนังสือรับรองการใช้คำหน้านามหญิง (ถ้ามี)',
-    files: [],
-  },
-];
-
 @UntilDestroy()
 @Component({
   selector: 'self-service-license-edit',
@@ -68,21 +41,19 @@ const FILE_LIST: FileGroup[] = [
   styleUrls: ['./license-edit.component.css'],
 })
 export class LicenseEditComponent implements OnInit {
-  prefixList$!: Observable<any>;
-
-  form = this.fb.group({
-    userInfo: [],
-  });
-
+  prefixList$!: Observable<Prefix[]>;
   oldValue: any;
-
-  uploadFileList: any[] = [];
-
+  uploadFileList: FileGroup[] = [];
   uniqueTimestamp!: string;
   requestId!: number;
   requestData!: SelfRequest;
   requestNo: string | null = '';
   currentProcess!: number;
+  myLicense = new SelfLicense();
+  form = this.fb.group({
+    userInfo: [],
+    editData: [],
+  });
 
   constructor(
     public dialog: MatDialog,
@@ -96,12 +67,21 @@ export class LicenseEditComponent implements OnInit {
 
   ngOnInit(): void {
     /* this.form.valueChanges.pipe(untilDestroyed(this)).subscribe((res) => {
-
     }); */
     this.getListData();
     this.checkRequestId();
-    // this.getMyInfo();
-    // this.initializeFile();
+  }
+
+  getMyLicense() {
+    const idcardno = getCookie('idCardNo');
+    this.myInfoService.getMyLicense(idcardno).subscribe((res) => {
+      if (res) {
+        this.myLicense = res[0];
+        this.form.controls.userInfo.patchValue(<any>this.myLicense);
+        this.oldValue = this.myLicense;
+        //console.log('my license = ', this.myLicense);
+      }
+    });
   }
 
   checkRequestId() {
@@ -111,34 +91,32 @@ export class LicenseEditComponent implements OnInit {
         // this.loadRequestFromId(this.requestId);
         this.requestService.getRequestById(this.requestId).subscribe((res) => {
           if (res) {
-            console.log(res);
+            //console.log(res);
             this.requestData = res;
             this.requestNo = res.requestno;
             this.currentProcess = Number(res.process);
             this.uniqueTimestamp = res.uniqueno || '';
-            console.log(this.uniqueTimestamp);
-
             this.patchData(res);
           }
         });
       } else {
         this.initializeFile();
-        this.getMyInfo();
+        this.getMyLicense();
       }
     });
   }
 
   patchData(data: SelfRequest) {
-    console.log(data);
+    //console.log(data);
     if (data.replacereasoninfo) {
       const replaceReasonInfo = parseJson(data.replacereasoninfo);
-      console.log(replaceReasonInfo);
-      this.form.controls.userInfo.patchValue(replaceReasonInfo);
+      //console.log(replaceReasonInfo);
+      this.form.controls.editData.patchValue(replaceReasonInfo);
     }
 
     if (data.fileinfo) {
       const fileInfo = parseJson(data.fileinfo);
-      console.log(fileInfo);
+      //console.log(fileInfo);
       const { attachfiles } = fileInfo;
       this.uploadFileList = attachfiles;
     }
@@ -153,15 +131,6 @@ export class LicenseEditComponent implements OnInit {
     this.prefixList$ = this.generalInfoService.getPrefix();
   }
 
-  getMyInfo() {
-    this.myInfoService.getMyInfo().subscribe((res) => {
-      console.log(res);
-      if (res) {
-        this.oldValue = res;
-      }
-    });
-  }
-
   navigateBack() {
     this.router.navigate(['/home']);
   }
@@ -174,7 +143,7 @@ export class LicenseEditComponent implements OnInit {
       addressinfo,
       schooladdrinfo,
       birthdate,
-      ...rawUserInfo
+      ...rawData
     } = this.oldValue || {
       id: null,
       updatedate: null,
@@ -182,7 +151,7 @@ export class LicenseEditComponent implements OnInit {
       schooladdrinfo: null,
       birthdate: null,
     };
-    const userInfo = toLowercaseProp(rawUserInfo);
+    const data = toLowercaseProp(rawData);
     const type = SelfServiceRequestSubType.อื่นๆ;
 
     const self = new SelfRequest(
@@ -200,10 +169,10 @@ export class LicenseEditComponent implements OnInit {
     const attachfiles = this.uploadFileList;
 
     const initialPayload = {
-      ...replaceEmptyWithNull(userInfo),
+      ...replaceEmptyWithNull(data),
       ...(this.requestId && { id: `${this.requestId}` }),
       ...{
-        replacereasoninfo: JSON.stringify({ ...formData.userInfo }),
+        replacereasoninfo: JSON.stringify({ ...formData.editData }),
       },
       ...{ fileinfo: JSON.stringify({ attachfiles }) },
     };
@@ -214,9 +183,8 @@ export class LicenseEditComponent implements OnInit {
   }
 
   onConfirm() {
-    console.log(this.form.getRawValue());
+    //console.log(this.form.getRawValue());
     const dialog = this.dialog.open(ConfirmDialogComponent, {
-      width: '350px',
       data: {
         title: `คุณต้องการยืนยันข้อมูลใช่หรือไม่`,
         subTitle: `คุณต้องการบันทึกข้อมูลและยื่นแบบคำขอใช่หรือไม่`,
@@ -247,22 +215,23 @@ export class LicenseEditComponent implements OnInit {
           ? this.requestService.updateRequest.bind(this.requestService)
           : this.requestService.createRequest.bind(this.requestService);
         request(payload).subscribe((res) => {
-          console.log('request result = ', res);
+          //console.log('request result = ', res);
           if (res.returncode === '00') {
-            this.onSaveAndRequest();
+            this.onSaveAndRequest(res);
           }
         });
       }
     });
   }
 
-  onSaveAndRequest() {
+  onSaveAndRequest(res: any) {
+    const today = new Date();
     const dialog = this.dialog.open(CompleteDialogComponent, {
       width: '375px',
       data: {
         header: `บันทึกข้อมูลและยื่นแบบคำขอสำเร็จเรียบร้อย`,
-        content: `วันที่ : 22 พฤศจิกายน 2565
-        เลขที่แบบคำขอ : SF_ED_12234467876543 `,
+        content: `วันที่ : ${thaiDate(today)}
+        เลขที่แบบคำขอ : SF_ED_12234467876543 `, // res.requestno
         subContent: `กรุณาตรวจสอบสถานะแบบคำขอหรือรหัสเข้าใช้งาน
           ผ่านทางอีเมลผู้ที่ลงทะเบียนภายใน 3 วันทำการ`,
       },
@@ -284,7 +253,6 @@ export class LicenseEditComponent implements OnInit {
 
   cancel() {
     const confirmDialog = this.dialog.open(ConfirmDialogComponent, {
-      width: '350px',
       data: {
         title: `คุณต้องการยกเลิกรายการแบบคำขอ
         ใช่หรือไม่? `,
@@ -310,7 +278,7 @@ export class LicenseEditComponent implements OnInit {
         id: `${this.requestId}`,
         isclose: '1',
       };
-      console.log('close request = ');
+      //console.log('close request = ');
       this.requestService.closeRequest(closePayload).subscribe((res) => {
         this.cancelCompleted();
       });
@@ -319,7 +287,6 @@ export class LicenseEditComponent implements OnInit {
 
   cancelCompleted() {
     const completeDialog = this.dialog.open(CompleteDialogComponent, {
-      width: '350px',
       data: {
         header: `ยกเลิกแบบคำขอสำเร็จ`,
       },
@@ -341,3 +308,18 @@ export type controlName =
   | 'lastnameTh'
   | 'lastnameEn'
   | 'distributeData';
+
+const FILE_LIST: FileGroup[] = [
+  {
+    name: '1. สำเนาหนังสือสำคัญการเปลี่ยนชื่อ/ชื่อสกุล/เปลี่ยนหรือเพิ่มคำนำหน้าชื่อ',
+    files: [],
+  },
+  {
+    name: '2. สำเนาหลักฐานการสมรส หรือการสิ้นสุดการสมรส (ถ้ามี)',
+    files: [],
+  },
+  {
+    name: '3. สำเนาหนังสือรับรองการใช้คำหน้านามหญิง (ถ้ามี)',
+    files: [],
+  },
+];
