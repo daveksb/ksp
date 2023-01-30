2840e5d0625467b730a05048295ca7dfa18b3742
daveksb <flying.geez@gmail.com>
2023-01-30 13:45:31 +0700

    duplicate nationality

--- libs/shared/utility/src/lib/default-form.ts
+++ libs/shared/utility/src/lib/default-form.ts
@@ -54,7 +54,7 @@ export function createUserInfoForm(fb: FormBuilder) {
       [Validators.required, Validators.pattern(phonePattern)],
     ],
     workphone: [null, [Validators.required, Validators.pattern(phonePattern)]],
-    nationality: ['TH'],
+    nationality: [null],
     country: [null],
     visaclass: [null],
     visatype: [null],
