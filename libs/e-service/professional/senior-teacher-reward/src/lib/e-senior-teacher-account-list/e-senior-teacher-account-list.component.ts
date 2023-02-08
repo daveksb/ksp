8a970411103684749e2d945e50e1814bb87eb9ad
Nutti Saelor <leelorz6@gmail.com>
2023-02-08 22:36:54 +0700

    fix: fix senior teacher form for confirm flow

--- libs/e-service/professional/senior-teacher-reward/src/lib/e-senior-teacher-account-list/e-senior-teacher-account-list.component.ts
+++ libs/e-service/professional/senior-teacher-reward/src/lib/e-senior-teacher-account-list/e-senior-teacher-account-list.component.ts
@@ -3,7 +3,10 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
-import { SelfServiceRequestSubType } from '@ksp/shared/constant';
+import {
+  SelfServiceRequestSubType,
+  SelfServiceRequestType,
+} from '@ksp/shared/constant';
 import { SelfApproveListSearch } from '@ksp/shared/interface';
 import { ERequestService, LoaderService } from '@ksp/shared/service';
 import { Subject } from 'rxjs';
@@ -58,7 +61,7 @@ export class ESeniorTeacherAccountListComponent implements AfterViewInit {
       createdate: params.createdate,
       offset: '0',
       row: '500',
-      requesttype: '1',
+      requesttype: SelfServiceRequestType.ขอรับรางวัลครูอาวุโส,
     };
     this.requestService.searchSelfApproveList(payload).subscribe((res) => {
       this.dataSource.data = res.map((i) => {
