9f497229fa95cf12e97dc1146894435885882864
zardokar <nattaworn@hotmail.com>
2023-06-24 13:30:09 +0700

    [:hammer:] reopen landing page in eservice

--- libs/e-service/feature/landing-page/src/lib/landing-page/landing-page.component.ts
+++ libs/e-service/feature/landing-page/src/lib/landing-page/landing-page.component.ts
@@ -10,7 +10,7 @@ export class LandingPageComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.standard();
+    console.log();
   }
 
   elicense() {
